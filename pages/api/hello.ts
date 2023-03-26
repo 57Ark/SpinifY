cecd57aebb5f681dd058110f8d222ebd0ab338d3
Arkhip Vouba <vouba.arkhip@gmail.com>
2023-03-26 11:43:52 +0800

    FIX: trying to run python command

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,32 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { execSync } from "child_process";
 import type { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
-import { PythonShell } from "python-shell";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const scriptPath = path.join(process.cwd(), "scripts");
+  // const scriptPath = path.join(process.cwd(), "scripts/script.py");
 
-  let dataToSend = "";
-  const results = await PythonShell.run("script.py", {
-    mode: "text",
-    pythonPath: "python",
-    pythonOptions: ["-u"],
-    scriptPath,
-    args: ["node.js", "python"],
-  });
+  // const dataToSend = "";
+  // const results = await PythonShell.run("script.py", {
+  //   mode: "text",
+  //   pythonPath: "python",
+  //   pythonOptions: ["-u"],
+  //   scriptPath,
+  //   args: ["node.js", "python"],
+  // });
   // const python = spawn("python", [scriptPath, "node.js", "python"]);
+  // const python = spawn(`python -c "print(2 + 3)"`, [""]);
+  const pythonProcess = execSync('python -c "print(2 + 3)"');
+
+  console.log(pythonProcess.toString());
 
   // for await (const data of python.stdout) {
   //   console.log(data.toString());
   //   dataToSend += data.toString();
   // }
-  for await (const data of results) {
-    console.log(data.toString());
-    dataToSend += data.toString();
-  }
+  // for await (const data of results) {
+  //   console.log(data.toString());
+  //   dataToSend += data.toString();
+  // }
 
-  return res.status(200).send({ data: dataToSend });
+  return res.status(200).send({ data: pythonProcess.toString() });
 }
